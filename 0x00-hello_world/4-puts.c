f802d80e28d138ec9d9bbcc878740f6554ba86c8
Emmanuel Juliet Olivia <emmanueljuliet2019@gmail.com>
2022-12-22 11:08:31 -0800

    olivia

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,7 @@
+#!/bin/bash
 #include <stdio.h>
-/**                                                                                                                                     
- *  * main - Entry                                                                                                                         
- *   * Return: Always 0                                                                                                                     
- *    */                                                                                                                                     
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	        puts("\"Programming is like building a multi lingual puzzle);                                                            
-			        return (0);
-}      
+
+int main(void) {
+	  puts("Programming is like building a multilingual puzzle");
+	    return 0;
+}
