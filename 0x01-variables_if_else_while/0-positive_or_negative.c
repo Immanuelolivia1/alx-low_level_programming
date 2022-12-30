adfc03c387a15d4e12aca7f1d6bbbb591d53d347
Emmanuel Juliet Olivia <emmanueljuliet2019@gmail.com>
2022-12-30 00:01:41 -0800

    olivia

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,7 +5,7 @@
 /**
  *  * main - print a random num
  *   * Return: Always 0
- **/
+**/
 int main(void)
 {	int n;
 
@@ -13,15 +13,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
-	
 		printf("%d is positive\n", n);
 
 	else if (n < 0)
-	
 		printf("%d is negative\n", n);
 	else
-								        
-	
 		printf("%d is zero\n", n);
 							
 	return (0);
